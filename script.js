64fa4e49d3ed89accf5654d241d4f64e4e92b133
litienyi <litienyi.work@gmail.com>
2023-06-26 18:18:56 +0800

    Version 0.3 (Development)

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // CONTAINER TO INCLUDE ALL JQUERY CODE
 $(document).ready(function(){
   const jsonText = $.getJSON("https://api.crossref.org/works?query.bibliographic=%22Chinese+Architecture%22&rows=2", function() {
-    console.log(jsonText);
-    console.log(`type of jsonText is ${typeof jsonText}`);
-    console.log(jsonText.responseJSON.message.items);
-    console.log(typeof jsonText.responseJSON);
     for (const item of jsonText.responseJSON.message.items) {
       // CREATE DIV ELEMENT
-      let newDiv = "<div>"
+      let newDiv = "<div class='newDiv'>"
         for (const [key, value] of Object.entries(item)) {
-          let newP = `<div class="newP"><div>${key}</div><div>${value}</div></div>`;
+          valueString = JSON.stringify(value);
+          console.log(value);
+          console.log(typeof value);
+          console.log(valueString);
+          console.log(typeof valueString);
+          let newP = `<div class='newP'><p class='objProperty'>${key}</p><p>${valueString}</p></div>`;
           newDiv += newP;
         }
       newDiv += "</div>"
