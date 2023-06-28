9d998d1206683571fea15d693c3cf8d732bee0df
litienyi <litienyi.work@gmail.com>
2023-06-28 22:54:17 +0800

    Version 1.1 (Beta)

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 INTRODUCTION
 This JSON Processor project is intended as a simple web application for visualizing, filtering, or otherwise processing JSON data. In particular, it is catered to JSON data collected through the CrossRef API.
 
-Current Version: 1.0 (Beta)
+Current Version: 1.1 (Beta)
 Version Notes:
-Version 1.0 is a funcitonal MVP. Users are able to query the CrossRef database from home.php, and results will show on results.html.
+Added filter function, which allows users to view select Object Properties.
 
 To look forward in the next version:
-The next version will incorporate a search bar and a filter function on results.html.
+A search bar on results.html; A function to view unique Object Values in select Properties (e.g. unique titles); A function to view distribution of "references-count" value.
--- resultsScript.js
+++ resultsScript.js
@@ -1,41 +1,94 @@
-function ajaxCall (userQuery) {
-    window.CrossRefResponse;
-    CrossRefResponse = $.getJSON(`https://api.crossref.org/works?query.bibliographic=${userQuery}&rows=101`, function () {
-      countResults (CrossRefResponse);
-      showResults(CrossRefResponse);
-      console.log("done");
-    });
-}
-
 $(document).ready(function() {
   let userQuery = window.location.search.split(/\?userQuery=/g)[1];
   ajaxCall (userQuery);
 });
 
-function countResults (CrossRefResponse) {
-  $(".query-summary").append(
-  `<p>Number of results: ${CrossRefResponse.responseJSON.message['total-results']}</p>`);
-  console.log("done querySummary")
+function ajaxCall (userQuery, propertyFilter) {
+  window.CrossRefResponse;
+  CrossRefResponseTEMP = $.getJSON(`https://api.crossref.org/works?query.bibliographic=${userQuery}&rows=10`, function () {
+    CrossRefResponse = CrossRefResponseTEMP["responseJSON"]["message"]["items"];
+    showProperties (CrossRefResponse);
+    countResults (CrossRefResponse, propertyFilter);
+    showResults(CrossRefResponse, propertyFilter);
+  });
 }
 
-function showResults(CrossRefResponse) {
-  if (CrossRefResponse.responseJSON.message.items.length < 100) {
-    for (const item of CrossRefResponse.responseJSON.message.items) {
+function showProperties (CrossRefResponse) {
+  let propertiesContent = "";
+  let propertiesList = [];
+  for (const item of CrossRefResponse) {
+    for (const property of Object.keys(item)) {
+      if (!propertiesList.includes(property)) {
+        propertiesList.push(property);
+      };
+    }
+  }
+  for (const property of propertiesList) {
+    propertiesContent += `<div><input class="filter-checkbox" type="checkbox" name="property" value="${property}">${property}</div>`
+  }
+  $(".properties-div").html(propertiesContent);
+  document.getElementById("filterProperties").addEventListener("click", filterResults);
+}
+
+function countResults (CrossRefResponse, propertyFilterList) {
+  if (propertyFilterList !== undefined) {
+    for (object of CrossRefResponse) {
+      for (property of Object.keys(object)) {
+        if (!propertyFilterList.includes(property)) {
+          delete object[property];
+        }
+      }
+      for (property of propertyFilterList) {
+        if (!Object.keys(object).includes(property)) {
+          object["property"] = "undefined";
+        }
+      }
+  }};
+  $(".query-summary").html(
+  `<p>Number of results: ${CrossRefResponse.length}</p>`);
+}
+
+function showResults(CrossRefResponse, propertyFilterList) {
+  if (propertyFilterList !== undefined) {
+    for (object of CrossRefResponse) {
+      for (property of Object.keys(object)) {
+        if (!propertyFilterList.includes(property)) {
+          delete object[property];
+        }
+      }
+      for (property of propertyFilterList) {
+        if (!Object.keys(object).includes(property)) {
+          object[property] = "undefined";;
+        }
+      }
+  }};
+  if (CrossRefResponse.length < 100) {
+    let DivContent = "";
+    for (const item of CrossRefResponse) {
       // CREATE DIV ELEMENT
       let newDiv = "<div class='newDiv'>"
         for (const [key, value] of Object.entries(item)) {
           valueString = JSON.stringify(value);
-          console.log(value);
-          console.log(typeof value);
-          console.log(valueString);
-          console.log(typeof valueString);
           let newP = `<div class='newP'><p class='objProperty'>${key}</p><p>${valueString}</p></div>`;
           newDiv += newP;
         }
       newDiv += "</div>"
-      $(".results-details").append(newDiv);
+      DivContent += newDiv
     }
-  } else {$(".results-details").append("<p>Your query returned more than 100 results. Results are hidden to prevent crashing.</p>");
-  console.log("done showResults");
+    $(".results-details").html(DivContent);
+  } else {$(".results-details").html("<p>Your query returned more than 100 results. Results are hidden to prevent crashing.</p>");
   }
+}
+
+function filterResults () {
+  let propertyFilterDOM = document.querySelectorAll('.filter-checkbox:checked');
+  let propertyFilterList = [];
+  for (propertyFilter of propertyFilterDOM) {
+    propertyFilterList.push(propertyFilter.value)
+  }
+  // propertyFilter = String(propertyFilterList);
+
+  let userQuery = window.location.search.split(/\?userQuery=/g)[1];
+
+  ajaxCall(userQuery, propertyFilterList);
 }
